e0ab99b73f3782d8c5728d7f5922e9608d11f434
wooobo <irooori89@gmail.com>
2021-11-15 10:20:52 +0900

    feat : post-examples 추가

---
+++ post-examples/src/main/java/com/example/postexamples/PostExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.postexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostExamplesApplication.class, args);
+    }
+
+}
---
+++ post-examples/src/test/java/com/example/postexamples/PostExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
